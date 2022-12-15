65755d25827f3e8dfe33204d52e0ae978519f66f
itai gendler <itaigendler@itais-MBP.lan>
2022-12-15 11:57:34 +0200

    wip

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/api/resource-x', (req, res) => {
-    res.status(400);
-    res.json({status: 400, data: "very BAD!"});
+    res.status(200);
+    res.json({status: 200, data: "all GOOD!"});
 });
 
 app.get('/api/resource-y', (req, res) => {
     res.status(400);
-    res.json({status: 400, data: "all GOOD!"});
+    res.json({status: 400, data: "very BAD!"});
 });
 
 app.listen(PORT, HOST, () => {
